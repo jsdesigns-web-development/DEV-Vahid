110e60c70a1761dc76595ce9bddbe72b53ed7f7b
vaheed-sediqi <vaheed,sediqi@gmail.com>
2019-08-05 21:45:59 +0200

    git issu solved

--- frs-jeannepatrickinsurance.com/js/owl.carousel.js
+++ frs-jeannepatrickinsurance.com/js/owl.carousel.js
@@ -2589,11 +2589,7 @@
 	 */
 	Autoplay.Defaults = {
 		autoplay: false,
-<<<<<<< HEAD
 		autoplayTimeout: 10000,
-=======
-		autoplayTimeout: 7000,
->>>>>>> 8d6e6e21353980e10cfc1ccf727628f285943dee
 		autoplayHoverPause: false,
 		autoplaySpeed: false
 	};
