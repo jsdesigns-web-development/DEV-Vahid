f9ad3eb8afd680dc36eada6e48c174b984469506
Brian Jason <holaymolay@Brians-MacBook-Pro.local>
2020-07-20 09:53:00 -0700

    init frs-myprovidence.net

--- frs-reliefnursingsf.com/js/main.js
+++
@@ -1,9 +0,0 @@
-function checkWidth() {
-    if($(window).width() < 992){
-        $('#line1').show();
-        $('#line2').show();
-    } else {
-        $('#line1').hide();
-        $('#line2').hide();
-    }
-}
